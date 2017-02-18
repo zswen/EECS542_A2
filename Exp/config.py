0709ec718ad0265509b8a1012cd4298198dea67b
Zhangsu Wen <zswen@0587377531.wireless.umich.net>
2017-02-18 15:05:17 -0500

    add dictionary

--- Exp/config.py
+++ Exp/config.py
@@ -1,1 +1,5 @@
 import numpy as np
+
+dict = {'aeroplane':1,'bicycle':2,'bird':3,'boat':4,'bottle':5,'bus':6,'car':7,
+				'cat':8,'chair':9,'cow':10,'diningtable':11,'dog':12,'horse':13,'motorbike':14,
+					'person':15,'potted plant':16,'sheep':17,'sofa':18,'train':19,'tv':20,'monitor':20}
