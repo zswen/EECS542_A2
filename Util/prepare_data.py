68ab4261570373120fb45a8023999884c6add831
Zhangsu Wen <zswen@ZhangsudeMacBook-Pro.local>
2017-02-18 20:14:38 -0500

    change getDetectionLabel's input

--- Util/prepare_data.py
+++ Util/prepare_data.py
@@ -3,9 +3,8 @@ import cv2
 import xml.etree.ElementTree as et
 import numpy as np
 
-def generateSegLabel(image_names, color2class):
+def generateSegLabel(image_names, color2class, dir_name):
 	labels = []
-	dir_name = '../TrainVal/VOCdevkit/VOC2011/SegmentationClass'
 	for idx, name in enumerate(image_names):
 		path = os.path.join(dir_name, name)
 		img = cv2.imread(path)
@@ -18,18 +17,46 @@ def generateSegLabel(image_names, color2class):
 	return np.array(labels)
 
 #0~20 classes, 0 for background
-def getDetectionLabel(filename, dict, off_value = -1):
-	vec = off_value * np.ones(len(dict) + 1) # add one for background
-	tree = et.parse(filename)
-	root = tree.getroot()
-	for subcatagory in root:
-		obj = subcatagory.find('name')
-		if obj != None:
-			vec[dict[obj.text]] = 1
-	return vec
+def getDetectionLabel(filenames, dir_name, dict, off_value = -1):
+	labels = []
+	for file in enumerate(filenames):
+		path = os.path.join(dir_name, file)
+		vec = off_value * np.ones(len(dict) + 1) # add one for background
+		tree = et.parse(path)
+		root = tree.getroot()
+		for subcatagory in root:
+			obj = subcatagory.find('name')
+			if obj != None:
+				vec[dict[obj.text]] = 1
+		labels.append(vec)
+	return labels
 
 def main():
-	generate_seg_label(['2007_000032.png'], {})
+	className2Idx = OrderedDict(
+	   {'aeroplane':1, 
+		'bicycle':2, 
+		'bird':3, 
+		'boat':4, 
+		'bottle':5, 
+		'bus':6, 
+		'car':7, 
+		'cat':8, 
+		'chair':9, 
+		'cow':10, 
+		'diningtable':11, 
+		'dog':12, 
+		'horse':13, 
+		'motorbike':14, 
+		'person':15, 
+		'pottedplant':16, 
+		'sheep':17, 
+		'sofa':18, 
+		'train':19, 
+		'tvmonitor':20
+		})
+	#generate_seg_label(['2007_000032.png'], {})
+	dir_name = '../TrainVal/VOCdevkit/VOC2011/SegmentationClass'
+	getDetectionLabel(['2007_000032.png'],dir_name,className2Idx)
 	return
 
 if __name__ == '__main__':
