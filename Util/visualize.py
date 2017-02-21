fd31ce9b265943af3ece88c4591ab44caa6987ad
Zhangsu Wen <zswen@ZhangsudeMacBook-Pro.local>
2017-02-20 21:29:40 -0500

    add visualization for debugging

--- Util/visualize.py
+++ Util/visualize.py
@@ -1,11 +1,35 @@
+import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+import pdb
+import os
 import sys
-sys.path.append('../Exp/config.py')
+from config import *
+from prepare_data import *
+sys.path.append('../Exp/')
 
-def visualize(image_mask, if_seg = True):
+def visualize(image_mask, if_seg = True, save_path = '../Submission/test.png'):
+
 	if if_seg:
-		return
+		[width, height] = image_mask.shape
+		seg_img = np.zeros((width, height, 3))
+		for i in range(width):
+			for j in range(height):
+				if image_mask[i, j] != 0:
+					seg_img[i, j, :] = idx2Color[int(image_mask[i, j])]
+
+		cv2.imwrite(save_path, seg_img)
 	else:
-		plt.imshow(image_mask)
+		cv2.imwrite(save_path, image_mask)
 	return
+
+
+def main():
+	path = '/Users/zswen/Desktop/EECS542/Assignment2_git/TrainVal/VOCdevkit/VOC2011/SegmentationClass'
+	seg = getSegLabel(['2007_000032'], color2Idx, path)
+	imput = np.asarray(seg[0])
+	visualize(imput, True)
+	return
+
+if __name__ == '__main__':
+	main()
