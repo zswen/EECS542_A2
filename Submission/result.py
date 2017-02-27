34e315a596fe8a3a4b31da6cb5da3ca040e5ffa9
Zhangsu Wen <zswen@2379590752.vpn.umich.net>
2017-02-27 00:41:34 -0500

    find wrong predictions

--- Submission/result.py
+++ Submission/result.py
@@ -46,6 +46,37 @@ def summary(img_list, current_img, locks, total_result_dict,
 				total_result_dict[k] = result_dict[k] #(i,j): integer
 		locks[1].release()
 
+def getWrongPrediction(result_dict):
+	wrong = {}
+	wrong_prediction = {}
+	for key,value in result_dict.items():
+		if key[0] != key[1]:
+			if not key[0] in wrong:
+				wrong[key[0]] = {}
+			wrong[key[0]][key[1]] = value
+
+	for key,value in wrong.items():
+		tp_dict = {}
+		for sub_key, sub_value in value.items():
+			if sub_key == 0:
+				tp_dict['background'] = sub_value
+			elif sub_key == 21:
+				tp_dict['uncertain'] = sub_value
+			else:
+				tp_dict[idx2ClassName[sub_key]] = sub_value
+		if key == 0:
+			wrong_prediction['background'] = tp_dict
+		elif key == 21:
+			wrong_prediction['uncertain'] = tp_dict
+		else:
+			wrong_prediction[idx2ClassName[key]] = tp_dict
+
+	print(wrong_prediction)
+
+
+			
+
+
 def calcMetric(result_dict): 
 	correct_pix = defaultdict(int)
 	total_pix = defaultdict(int)
@@ -98,13 +129,13 @@ def calcMetric(result_dict):
 
 
 def main():
-	f = open(os.path.join(data_split_root, 'val.txt'))
-	names = f.readlines()
-	val_ims = mgr.list([name[0: -1] for name in names])
-	total_result_dict = mgr.dict()
-	current_img = mgr.list([0])
-	processors = []
-	if not os.path.exists('summary.pickle'):
+	if not os.path.exists('summary.pkl'):
+		f = open(os.path.join(data_split_root, 'val.txt'))
+		names = f.readlines()
+		val_ims = mgr.list([name[0: -1] for name in names])
+		total_result_dict = mgr.dict()
+		current_img = mgr.list([0])
+		processors = []
 		for _ in range(10):
 			P = Process(target = summary,
 						args = (val_ims, 
@@ -116,13 +147,18 @@ def main():
 
 		for P in processors:
 			P.join()
-		pdb.set_trace()
-		with open('summary.pickle', 'wb') as handle:
-			pickle.dump(total_result_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
-		calcMetric(total_result_dict)
+		handle = open('summary.pkl', 'wb')
+		total_result_dict = dict(total_result_dict)
+		pickle.dump(total_result_dict, handle)
+		handle.close()
+		getWrongPrediction(result_dict)
+		#calcMetric(total_result_dict)
 	else:
-		with open('filename.pickle', 'rb') as handle:
-			calcMetric(pickle.load(handle))
+		handle = open('summary.pkl', 'rb')
+		result_dict = pickle.load(handle)
+		handle.close()
+		calcMetric(result_dict)
+		getWrongPrediction(result_dict)
 	return
 
 if __name__ == '__main__':
