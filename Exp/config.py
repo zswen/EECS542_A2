fix gradients bug, add test features

Exp/config.py
@@ -3,20 +3,20 @@ from collections import OrderedDict
 
 learning_rate = 1e-5
 upsample_mode = 16
-model_init = 'partialResize_16'
-model_save = 'partialResize_%d' % upsample_mode
+model_init = 'partialResize_accu_16'
+model_save = 'partialResize_accu_%d' % upsample_mode
 
 bias = None#np.ones(22)
-train_mode = True #False for testing
+train_mode = False #False for testing
 optimizer_mode = 'Mome' #Support [Adam/Mome]
-result_save_path = '../Submission/Result_%d' % upsample_mode
+result_save_path = '../Submission/Result_accu_%d' % upsample_mode
 batch_size = 20
 snapshot_iter = 100 #write checkpoint for every @ iterations
-data_loader_capacity = 10 #cache how many batches
+data_loader_capacity = 20 #cache how many batches
 resize_threshold = 100
 max_iter = float('inf')
 num_processor = 4 #number of processes loading data
-silent_train = True #print loss for every step
+silent_train = False #print loss for every step
 
 image_root = '../TrainVal/VOCdevkit/VOC2011/JPEGImages'
 annotation_root = '../TrainVal/VOCdevkit/VOC2011/Annotations'

Exp/main.py
@@ -10,6 +10,7 @@ from step import *
 from config import *
 sys.path.append('../Util')
 from debug import loadDataOneThread
+from prepare_data import getSegLabel
 from visualize import visualize
 import pdb
 
@@ -104,14 +105,20 @@ def main():
 			print('[$] iter timing: %d' % (time.clock() - t0))
 	#start testing
 	else:
+		total_loss = []
 		if not os.path.isdir(result_save_path):
 			os.makedirs(result_save_path)
 			print('[!]Direction for results not found\n[!]Create %s' % result_save_path)
 		for idx, img_name in enumerate(data_ims):
 			if not os.path.isfile(os.path.join(result_save_path, img_name + '.png')):
 				img = cv2.imread(os.path.join(image_root, img_name + '.jpg'))
-				[segmentation] = sess.run([net.pred_up], 
-					feed_dict = {net.im_input: np.array([img])})
+				label = getSegLabel([img_name], color2Idx, segmentation_root)
+				[segmentation, loss] = sess.run([net.pred_up, net.loss], 
+					feed_dict = {net.im_input: np.array([img]), 
+								 net.seg_label: np.array(label)})
+				total_loss.append(loss)
+				if idx % 1 == 0:
+					print('[#]Test Loss: %f' % loss)
 				visualize(segmentation[0, ...], \
 					if_seg = True, save_path = os.path.join(result_save_path, img_name + '.png'))
 				print('Save segmentation result of     %s, [%d/%d]' \
@@ -119,7 +126,7 @@ def main():
 			else:
 				print('Detected segmentation result of %s, [%d/%d]' \
 					% (img_name, idx + 1, len(data_ims)))
-
+		print('ALL IMAGES DONE, TOTAL LOSS: %f' % np.mean(total_loss))
 	return
 	
 

Exp/step.py
@@ -58,14 +58,18 @@ def step(sess, net, data_loader, cv_empty, cv_full, silent = True):
 	cv_full.notify()
 	cv_empty.release()
 	total_loss = []
+	sub_shapes = []
 	for idx, subbatch in enumerate(subbatches):
-		[loss, seg_loss, softmax, upscore32, score_fr, fc7, fc6, pool4, score_pool4, fuse_pool4, _] = \
-			sess.run([net.loss, net.cross_entropy_mean, net.softmax, net.upscore32, net.score_fr, net.fc7, net.fc6, net.pool4, net.score_pool4, net.fuse_pool4, net.train_op], \
+		sub_shapes.append(subbatch['images'].shape[0])
+		[loss, seg_loss, _] = \
+			sess.run([net.loss, net.cross_entropy_mean, net.enqueue], \
 			  				  feed_dict = {net.im_input: np.array(subbatch['images']),
-			  			   	   			   net.seg_label: np.array(subbatch['labels']),
-			  			   	   			   net.apply_grads_flag: int(idx == len(subbatches) - 1)})
+			  			   	   			   net.seg_label: np.array(subbatch['labels'])})
 		total_loss.append(seg_loss)
-	net.done_optimize()
+	#optimization
+	sess.run([net.train_op], feed_dict = {net.apply_grads_num: np.array(sub_shapes),
+											   net.batch_num: np.array([len(sub_shapes)])})
+
 	if not silent:
 		print('\t[!]segmentation loss: %f, total loss: %f' % (seg_loss, loss))
 	return np.mean(total_loss)
@@ -88,9 +92,7 @@ def main():
 		test_label = getSegLabel([f], color2Idx, segmentation_root)
 		[prediction, loss, conv5_3] = sess.run([net.pred_up, net.loss, net.conv5_3], \
 						  				   feed_dict = {net.im_input: np.array([test_img]),
-						  			   	   				net.seg_label: np.array([test_label]),
-						  			   	   				net.apply_grads_flag: idx % 2})
-		pdb.set_trace()
+						  			   	   				net.seg_label: np.array([test_label])})
 	print(loss)
 	cv2.imwrite('test.png', prediction[0, ...])
 	return

Network/fcn.py
@@ -32,17 +32,18 @@ class FCN32VGG(Model):
             sys.exit(1)
 
         self.data_dict = np.load(vgg16_npy_path, encoding='latin1').item()
-        self.wd = 5e-4
-        self.gradients_pool = []
+        self.wd = 1e-3
+        self.gradients_pool = {}
         self.average_grads = []
         self.im_input = tf.placeholder(tf.float32)
         self.seg_label = tf.placeholder(tf.int32)
-        self.apply_grads_flag = tf.placeholder(tf.int32)
+        self.apply_grads_num = tf.placeholder(tf.float32)
+        self.batch_num = tf.placeholder(tf.int32, shape = [1])
         self.varlist = []
         print("npy file loaded")
 
     def build(self, train, upsample_mode = 32, num_classes = 22, 
-                random_init_fc8 = False, debug = False):
+            capacity = 10, random_init_fc8 = False, debug = False):
         """
         Build the VGG model using loaded weights
         Parameters
@@ -62,6 +63,7 @@ class FCN32VGG(Model):
         # Convert RGB to BGR
 
         self.num_classes = num_classes
+        self.capacity = capacity
         self.global_step = tf.Variable(0, name = 'global_step', trainable = False)
         with tf.name_scope('Processing'):
 
@@ -205,33 +207,34 @@ class FCN32VGG(Model):
                 self.opt = tf.train.MomentumOptimizer(learning_rate = lr)
             else:
                 assert 0
+
+            #calculate gradients
             self.gradients = self.opt.compute_gradients(self.loss)
-            self.gradients_pool.append(self.gradients)
+            enqueue_ops = []
+            with tf.device('/cpu: 0'):
+                for grad_and_var in self.gradients:
+                    self.gradients_pool[grad_and_var[1]] = \
+                        tf.FIFOQueue(self.capacity, tf.float32, shapes = grad_and_var[0].get_shape())
+                    enqueue = self.gradients_pool[grad_and_var[1]].enqueue(grad_and_var[0])
+                    enqueue_ops.append(enqueue)
 
-            #self.train_op = self.opt.apply_gradients(self.gradients, 
-            #                          global_step = self.global_step)
+            self.enqueue = tf.group(*enqueue_ops)
+
+            for var in self.gradients_pool:
+                #grad_and_vars_i is (var0, grad0_imgi)
+                with tf.device('/cpu: 0'):
+                    grads = self.gradients_pool[var].dequeue_many(self.batch_num)
+                grad = tf.concat(grads, 0)
+                temp_grad_num = self.apply_grads_num
+                for _ in range(len(grad.get_shape()) - 1):
+                    temp_grad_num = tf.expand_dims(temp_grad_num, -1)
+                grad = tf.div(tf.reduce_mean(tf.multiply(grad, temp_grad_num), 0), 
+                    tf.reduce_mean(self.apply_grads_num))
+                grad_and_var = (grad, var)
+                self.average_grads.append(grad_and_var)
             
-            placeholder_op = lambda: tf.no_op()
-
-            self.train_op = tf.cond(tf.equal(self.apply_grads_flag, 1), self.optimize, placeholder_op)
-
-    def optimize(self):
-        #accumulate gradients
-        for grad_and_vars in zip(*self.gradients_pool):
-            #grad_and_vars_i is (var0, grad0_imgi)
-            grads = [tf.expand_dims(g, 0) for g, _ in grad_and_vars]
-            grad = tf.concat(grads, 0)
-            grad = tf.reduce_mean(grad, 0)
-            v = grad_and_vars[0][1]
-            grad_and_var = (grad, v)
-            self.average_grads.append(grad_and_var)
-
-        return self.opt.apply_gradients(self.average_grads, 
-                                        global_step = self.global_step)
-
-    def done_optimize(self):
-        self.average_grads = []
-        self.gradients_pool = []
+            self.train_op = self.opt.apply_gradients(self.average_grads, 
+                                      global_step = self.global_step)
 
     def _max_pool(self, bottom, name, debug):
         pool = tf.nn.max_pool(bottom, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1],